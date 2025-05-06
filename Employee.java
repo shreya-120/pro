23b90691e2a0034bcf3527ecf3655e627aed9db6
Shreya-120 <shreyasabannavar320@gmail.com>
2025-05-06 15:17:38 +0530

    Add new file to repo

---
+++ Employee.java
@@ -0,0 +1,23 @@
+public class Employee{  
+    String name;
+    int id;
+    double salary;
+    String city;
+    public Employee(String name, int id, double salary, String city){
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+        this.city = city;
+    }
+    public void displayInfo() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Name: " + name);
+        System.out.println("Salary: " + salary);
+    }
+    public static void main(String[] args){
+        // Creating an object of Employee
+        Employee emp1 = new Employee("XYZ", 101,50000.0,"Hubli");
+        emp1.displayInfo(); 
+    }
+} 
+
